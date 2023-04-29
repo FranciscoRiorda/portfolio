32cc448f51a4445f0d656512ad42a5bbd0e9978d
Francisco Riorda <riorda7@gmail.com>
2023-04-29 14:08:54 -0300

    form contacto

--- src/componentes/Contacto/Contacto.jsx
+++ src/componentes/Contacto/Contacto.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../Contacto/contactoStyles.css";
 import Maps from "../maps/maps";
+import emailjs from "@emailjs/browser";
+import Swal from 'sweetalert2'
 
 const Contacto = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_62ou68q",
+        "template_70n4sca",
+        form.current,
+        "9XljjtFcR3B5xdPma"
+      )
+      .then(
+        (result) => {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Correo enviado',
+            text: "Pronto me estaré contactanto",
+            showConfirmButton: false,
+            timer: 1500
+          })
+        },
+        (error) => {
+          Swal.fire({
+            position: 'center',
+            icon: 'warning',
+            title: 'Correo no enviado',
+            text: "Intenta nuevamente mas tarde",
+            showConfirmButton: false,
+            timer: 1500
+          })
+        }
+      );
+  };
+
   return (
     <>
       <div className="contacto">
@@ -12,23 +50,39 @@ const Contacto = () => {
           </div>
         </div>
         <div className="mapComponent">
-            <Maps className="maps"></Maps>
+          <Maps className="maps"></Maps>
         </div>
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <div className="formContacto">
-            <input className="nombreContacto" placeholder="Nombre"></input>
-            <input className="emailContacto" placeholder="Email"></input>
+            <input
+              className="nombreContacto"
+              placeholder="Nombre"
+              type="text"
+              name="user_name"
+              required
+            ></input>
+            <input
+              className="emailContacto"
+              placeholder="Email"
+              type="email"
+              name="user_email"
+              required
+            ></input>
             <textarea
+              name="message"
               placeholder="Mensaje.."
-              name=""
-              id=""
               cols="30"
               rows="10"
+              required
             ></textarea>
           </div>
-          <div className="botonSubmit">
-            <p type="submit">Enviar Mensaje</p>
+          <div>
+            <input
+              className="botonSubmit"
+              type="submit"
+              value="Enviar Mensaje"
+            />
           </div>
         </form>
       </div>
