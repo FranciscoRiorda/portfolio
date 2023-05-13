455d02343a6cccd401097e21e4c851f5d3b9dbf9
Francisco Riorda <riorda7@gmail.com>
2023-05-13 16:44:47 -0300

    theme provider

--- src/componentes/Home/Home.jsx
+++ src/componentes/Home/Home.jsx
@@ -1,56 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./homeStyles.css";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
+import { FaRegLightbulb } from "react-icons/fa";
+import { FaLightbulb } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+
+const [theme, setTheme] = useState(true);
+
+const changeTheme = () => {
+  setTheme(!theme);
+}
+
   return (
     <>
-      <div className="card">
-        <div className="container">
-          <div data-aos="fade-up" className="container2">
-            <div className="cardPerfil">
-              <div className="imgPerfil"></div>
+      <div>
+        <div className="themeButton">
+        <FaLightbulb class={`themeMode ${theme ? 'darkMode' : ''}`} onClick={changeTheme}></FaLightbulb>
+        <FaRegLightbulb class={`themeMode ${!theme ? 'whiteMode' : ''}`} onClick={changeTheme}></FaRegLightbulb>
+        </div>
+        <div className="card">
+          <div className="container">
+            <div data-aos="fade-up" className="container2">
+              <div className="cardPerfil">
+                <div className="imgPerfil"></div>
 
-              <div className="perfil">
-                <div data-aos="fade-left" className="nombre">
-                  <p>Francisco Riorda</p>
-                </div>
-                <div data-aos="fade-left" className="descripcion">
-                  <p>
-                    Desarrollador FrontEnd - Técnico Universitario en
-                    Programación UTN FRC
-                  </p>
-                </div>
-                <div data-aos="fade-left" className="redes">
-                  <div>
-                    <Link
-                      data-aos="fade-left"
-                      to="https://www.linkedin.com/in/francisco-riorda/"
-                      target="_blank"
-                    >
-                      <FaLinkedin className="redesHome" />
-                    </Link>
+                <div className="perfil">
+                  <div data-aos="fade-left" className="nombre">
+                    <p>Francisco Riorda</p>
                   </div>
-                  <div>
-                    <Link
-                      data-aos="fade-left"
-                      to="https://github.com/FranciscoRiorda"
-                      target="_blank"
-                    >
-                      <FaGithub className="redesHome" />
-                    </Link>
+                  <div data-aos="fade-left" className="descripcion">
+                    <p>
+                      Desarrollador FrontEnd - Técnico Universitario en
+                      Programación UTN FRC
+                    </p>
                   </div>
-                  <div>
-                    <Link
-                      data-aos="fade-left"
-                      to="https://wa.me/3534795305"
-                      target="_blank"
-                    >
-                      <FaWhatsapp className="redesHome" />
-                    </Link>
+                  <div data-aos="fade-left" className="redes">
+                    <div>
+                      <Link
+                        data-aos="fade-left"
+                        to="https://www.linkedin.com/in/francisco-riorda/"
+                        target="_blank"
+                      >
+                        <FaLinkedin className="redesHome" />
+                      </Link>
+                    </div>
+                    <div>
+                      <Link
+                        data-aos="fade-left"
+                        to="https://github.com/FranciscoRiorda"
+                        target="_blank"
+                      >
+                        <FaGithub className="redesHome" />
+                      </Link>
+                    </div>
+                    <div>
+                      <Link
+                        data-aos="fade-left"
+                        to="https://wa.me/3534795305"
+                        target="_blank"
+                      >
+                        <FaWhatsapp className="redesHome" />
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
---
+++ src/contexts/theme.jsx
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from "react";
+
+export const Theme = createContext({});
+
+const ThemeProvider = ({children}) => {
+
+    const [themeColor, setThemeColor] = useState('dark');
+
+    return(
+        <Theme.Provider value={{themeColor, setThemeColor}}>
+            {children}
+        </Theme.Provider>
+    )
+}
+
+export default ThemeProvider;
